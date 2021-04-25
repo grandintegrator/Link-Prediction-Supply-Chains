cfa944736db9d627061aa6d2b1100ecac103830c
grandintegrator <33590121+grandintegrator@users.noreply.github.com>
2021-04-25 15:28:31 +0800

    initial migratory commit

---
+++ src/modelling/seal_implementation.py
@@ -0,0 +1,225 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import scipy.sparse as sp
+import pandas as pd
+import dgl.function as fn
+import networkx as nx
+
+import pickle
+import dgl.data
+import logging
+import dgl
+import torch
+import itertools
+
+from tqdm import tqdm
+from dgl.nn import SAGEConv
+
+# Pandas debugging options
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
+# Logger preferences
+logger = logging.getLogger(__name__)
+logging.getLogger().setLevel(logging.INFO)
+logging.basicConfig()
+
+
+################################################################################
+# LOAD DATASET FROM NETWORKX
+################################################################################
+class DataLoaderDGL(object):
+    def __init__(self, path: str = 'data/02_intermediate/marklinesEdges.p'):
+        """Loads NetworkX graph from pickle dataset into a DGL Graph Object
+        Args:
+            path: Location of the MarkLines pickled object
+        """
+        logger.info('Loading NetworkX graph object into DGL')
+        # load existing graph object
+        self.multi_pair_frame = None
+        self.graph_object = pickle.load(open(path, "rb"))
+        logger.info('NetworkX graph loaded')
+
+    def get_dgl_graph(self):
+        return dgl.from_networkx(self.graph_object.G,
+                                 edge_attrs=None)
+
+
+data_loader = DataLoaderDGL()
+dataset = data_loader.get_dgl_graph()
+g = dataset
+
+EMBEDDING_INITIALISED = 15
+embed = nn.Embedding(g.number_of_nodes(), EMBEDDING_INITIALISED)
+g.ndata['feat'] = embed.weight
+
+################################################################################
+# Prepare training and testing sets
+################################################################################
+# Split edge set for training and testing
+u, v = g.edges()
+
+eids = np.arange(g.number_of_edges())
+eids = np.random.permutation(eids)
+test_size = int(len(eids) * 0.1)
+train_size = g.number_of_edges() - test_size
+test_pos_u, test_pos_v = u[eids[:test_size]], v[eids[:test_size]]
+train_pos_u, train_pos_v = u[eids[test_size:]], v[eids[test_size:]]
+
+# Find all negative edges and split them for training and testing
+adj = sp.coo_matrix((np.ones(len(u)), (u.numpy(), v.numpy())))
+adj_neg = 1 - adj.todense() - np.eye(g.number_of_nodes())
+neg_u, neg_v = np.where(adj_neg != 0)
+
+neg_eids = np.random.choice(len(neg_u), g.number_of_edges() // 2)
+test_neg_u, test_neg_v = (
+    neg_u[neg_eids[:test_size]],
+    neg_v[neg_eids[:test_size]]
+)
+train_neg_u, train_neg_v = (
+    neg_u[neg_eids[test_size:]],
+    neg_v[neg_eids[test_size:]]
+)
+
+train_g = dgl.remove_edges(g, eids[:test_size])
+test_g = dgl.remove_edges(g, eids[train_size:])
+
+################################################################################
+# MODEL - Two layers of GraphSAGEConv
+################################################################################
+class GraphSAGE(nn.Module):
+    def __init__(self, in_feats, h_feats):
+        super(GraphSAGE, self).__init__()
+        self.conv1 = SAGEConv(in_feats, h_feats, 'mean')
+        self.conv2 = SAGEConv(h_feats, h_feats, 'mean')
+        self.conv3 = SAGEConv(h_feats, h_feats, 'mean')
+
+    def forward(self, g, in_feat):
+        h = self.conv1(g, in_feat)
+        h = F.leaky_relu(h)
+        h = self.conv2(g, h)
+        h = F.leaky_relu(h)
+        h = self.conv3(g, h)
+        return h
+
+
+################################################################################
+# Create positive and negative graphs from the sets of nodes for easier compute
+################################################################################
+train_pos_g = dgl.graph((train_pos_u, train_pos_v), num_nodes=g.number_of_nodes())
+train_neg_g = dgl.graph((train_neg_u, train_neg_v), num_nodes=g.number_of_nodes())
+
+test_pos_g = dgl.graph((test_pos_u, test_pos_v), num_nodes=g.number_of_nodes())
+test_neg_g = dgl.graph((test_neg_u, test_neg_v), num_nodes=g.number_of_nodes())
+
+
+################################################################################
+# The benefit of treating the pairs of nodes as a graph is that you can
+# use the ``DGLGraph.apply_edges`` method, which conveniently computes new
+# # edge features based on the incident nodes’ features and the original
+# # edge features (if applicable).
+#
+# DGL provides a set of optimized builtin functions to compute new
+# edge features based on the original node/edge features. For example,
+# ``dgl.function.u_dot_v`` computes a dot product of the incident nodes’
+# representations for each edge.
+################################################################################
+class DotPredictor(nn.Module):
+    def forward(self, g, h):
+        with g.local_scope():
+            g.ndata['h'] = h
+            # Compute a new edge feature named 'score' by a dot-product between the
+            # source node feature 'h' and destination node feature 'h'.
+            g.apply_edges(fn.u_dot_v('h', 'h', 'score'))
+            # u_dot_v returns a 1-element vector for each edge so you need to squeeze it.
+            return g.edata['score'][:, 0]
+
+
+######################################################################
+# You can also write your own function if it is complex.
+# For instance, the following module produces a scalar score on each edge
+# by concatenating the incident nodes’ features and passing it to an MLP.
+
+class MLPPredictor(nn.Module):
+    def __init__(self, h_feats):
+        super().__init__()
+        self.W1 = nn.Linear(h_feats * 2, h_feats)
+        self.W2 = nn.Linear(h_feats, 1)
+
+    def apply_edges(self, edges):
+        """
+        Computes a scalar score for each edge of the given graph.
+
+        Parameters
+        ----------
+        edges :
+            Has three members ``src``, ``dst`` and ``data``, each of
+            which is a dictionary representing the features of the
+            source nodes, the destination nodes, and the edges
+            themselves.
+
+        Returns
+        -------
+        dict
+            A dictionary of new edge features.
+        """
+        h = torch.cat([edges.src['h'], edges.dst['h']], 1)
+        return {'score': self.W2(F.relu(self.W1(h))).squeeze(1)}
+
+    def forward(self, g, h):
+        with g.local_scope():
+            g.ndata['h'] = h
+            g.apply_edges(self.apply_edges)
+            return g.edata['score']
+
+
+model = GraphSAGE(train_g.ndata['feat'].shape[1], 16)
+pred = DotPredictor()
+
+
+def compute_loss(pos_score, neg_score):
+    scores = torch.cat([pos_score, neg_score])
+    labels = torch.cat([torch.ones(pos_score.shape[0]), torch.zeros(neg_score.shape[0])])
+    return F.binary_cross_entropy_with_logits(scores, labels)
+
+
+def compute_auc_ap(pos_score, neg_score):
+    scores = torch.cat([pos_score, neg_score]).numpy()
+    labels = torch.cat(
+        [torch.ones(pos_score.shape[0]), torch.zeros(neg_score.shape[0])]).numpy()
+    return [roc_auc_score(labels, scores),
+            average_precision_score(labels, scores)]
+
+
+optimizer = torch.optim.Adam(itertools.chain(model.parameters(),
+                                             pred.parameters()), lr=0.01)
+
+all_logits = []
+for e in range(2000):
+    # forward
+    h = model(train_g, train_g.ndata['feat'])
+    pos_score = pred(train_pos_g, h)
+    neg_score = pred(train_neg_g, h)
+    loss = compute_loss(pos_score, neg_score)
+
+    # backward
+    optimizer.zero_grad()
+    loss.backward(retain_graph=True)
+    optimizer.step()
+
+    if e % 5 == 0:
+        logger.info('In epoch {}, loss: {}'.format(e, loss))
+
+# ----------- 5. check results ------------------------ #
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import average_precision_score
+
+with torch.no_grad():
+    pos_score = pred(test_pos_g, h)
+    neg_score = pred(test_neg_g, h)
+    auc, av_precision = compute_auc_ap(pos_score, neg_score)
+    logger.info(f'Average AUC at Final: {auc}')
+    logger.info(f'Average Precision: {av_precision}')
+    # print('AUC at final Epoch', compute_auc_ap(pos_score, neg_score))
---
+++ src/utils.py
@@ -0,0 +1,253 @@
+from cleanco import prepare_terms, basename
+from datetime import datetime
+import random
+from tqdm import tqdm
+import networkx as nx
+import numpy as np
+from sklearn.metrics import classification_report, log_loss, roc_curve, roc_auc_score, precision_recall_curve, \
+    average_precision_score
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import traceback
+from _thread import start_new_thread
+from functools import wraps
+
+import torch
+from torch.multiprocessing import Queue
+import dgl
+
+
+def getTime():
+    return datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+
+# return default value if error
+def get(function, arg, default):
+    try:
+        return function(arg)
+    except Exception as e:
+        return default
+
+
+"""
+PREPROCESSING
+"""
+
+
+def stripCompany(business_name, n=2):
+    # clean n times because some companies have multiple legal statuses
+
+    # clean terms
+    terms = prepare_terms()
+    newName = basename(business_name, terms, prefix=True, middle=False, suffix=True)
+
+    if n > 0:
+        return stripCompany(newName, n - 1)
+    else:
+        return newName
+
+
+def cleanCompany(name):
+    stripName = stripCompany(name)
+    stripName = stripName.upper()
+    return stripName
+
+
+def cleanProduct(text, removeWords):
+    for word in removeWords:
+        text = text.replace(word, "")
+
+    return text
+
+
+# take in string list and return list
+def splitMarklinesList(textList):
+    if pd.isna(textList):
+        return []
+    else:
+        textList = textList.strip("\t").strip("\r\n")
+        textList = str(textList).split("\r\n")
+        textList = [_list.upper() for _list in textList]
+        return textList
+
+
+# TODO: DEPRECATED
+# take in string list and return list
+def splitCustomers(textList):
+    if pd.isna(textList):
+        return []
+    else:
+        textList = textList.strip("\t").strip("\r\n")
+        textList = str(textList).split("\r\n")
+        textList = [_list.upper() for _list in textList]
+        return textList
+
+
+"""
+SPLIT TRAIN-TEST
+"""
+
+
+def gen_train_test_extra(G, extraEdges):
+    # [RANDOM SAMPLING] GATHER TRAIN AND TEST DATA
+    # (node1, node2, isEdgeExist)
+    trainDataEdges = []
+    testDataEdges = []
+
+    count = 0
+    # sample edges
+    edges = list(G.edges())
+    M = len(edges)
+
+    # KEY: SHUFFLE THE TRAINING/TESTING EDGES
+    random.shuffle(edges)
+
+    for node1, node2 in tqdm(edges):
+        if count == M:
+            break
+
+        y = G.has_edge(node1, node2)
+
+        if count < int(M / 2):
+            trainDataEdges.append([node1, node2, y])
+        else:
+            testDataEdges.append([node1, node2, y])
+
+        count += 1
+
+    # sample non-edges
+    count = 0
+    testM = M  # 3*M
+    for node1, node2 in tqdm(nx.non_edges(G)):
+        # only keep M non-edges
+        if count == testM:
+            break
+
+        y = G.has_edge(node1, node2)
+
+        # selectively sample non-edge
+        if np.random.uniform() > 0.9:
+
+            if count < int(testM / 2):
+                trainDataEdges.append([node1, node2, y])
+            else:
+                testDataEdges.append([node1, node2, y])
+
+            count += 1
+
+    # sample non-edges
+    count = 0
+    # KEY: SHUFFLE THE TRAINING/TESTING EDGES
+    random.shuffle(edges)
+
+    for node1, neighNode in tqdm(edges):
+        # only keep M non-edges
+        if count == M:
+            break
+
+        # get neighbours from both sides
+        N2 = list(G.neighbors(neighNode))
+        random.shuffle(N2)
+
+        # find neighbors of N2 that are not connected to node1
+        node2 = None
+        for neigh in N2:
+            if (not G.has_edge(node1, neigh)) & (node1 != neigh):
+                node2 = neigh
+                break
+
+        if node2 == None:
+            continue
+
+        y = G.has_edge(node1, node2)
+
+        if count < int(M / 2):
+            trainDataEdges.append([node1, node2, y])
+        else:
+            testDataEdges.append([node1, node2, y])
+
+        count += 1
+
+    # extraEdges
+    extraDataEdges = []
+    for node1, node2 in tqdm(extraEdges):
+        y = G.has_edge(node1, node2)
+
+        extraDataEdges.append([node1, node2, y])
+
+    return trainDataEdges, testDataEdges, extraDataEdges
+
+
+"""
+RESULT ANALYSIS
+"""
+
+
+# evaluate dataframe results
+def evaluate(__df__, index, thresh=0.5):
+    try:
+        ## PURE HISTOGRAM
+        plt.figure(figsize=(10, 5))
+        for lab, scoreDF in __df__.groupby("label"):
+            c = "b" if lab == 1 else "r"
+            name = "edge" if lab == 1 else "non-edge"
+            plt.hist(scoreDF[index], color=c, alpha=0.3, label=name)
+
+        plt.legend()
+        plt.show()
+
+        ## CLASSIFICATION
+        predThres = __df__[index].apply(lambda x: x > thresh)
+        print(classification_report(__df__["label"], predThres))
+
+        ## AUC-ROC
+        y_true = __df__["label"]
+        y_score = __df__[index]
+
+        fig, (ax1, ax2) = plt.subplots(1, 2)
+        fig.set_figheight(10)
+        fig.set_figwidth(20)
+        fpr, tpr, thresholds = precision_recall_curve(y_true, y_score)
+        ax1.plot(fpr, tpr)
+        ax1.set_title("Precision-Recall Curve")
+        fpr, tpr, thresholds = roc_curve(y_true, y_score)
+        ax2.plot(fpr, tpr)
+        ax2.set_title("ROC Curve, AUC = {:.2f}".format(roc_auc_score(y_true, y_score)))
+        plt.show()
+    except Exception as e:
+        print(e)
+
+
+"""
+Compare All Results
+"""
+
+
+# evaluation function
+def plotCurves(__df__, columns):
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    fig.set_figheight(10)
+    fig.set_figwidth(20)
+
+    for index in columns:
+        ## AUC-ROC
+        y_true = __df__["label"]
+        y_score = __df__[index]
+
+        auc = roc_auc_score(y_true, y_score)
+
+        fpr, tpr, thresholds = precision_recall_curve(y_true, y_score)
+        #         ax1.plot(fpr, tpr, label=index)
+        ax1.plot(fpr, tpr, label="{}-{:.2f}".format(index, average_precision_score(y_true, y_score)))
+        ax1.set_title("Precision-Recall Curve")
+        ax1.legend()
+        fpr, tpr, thresholds = roc_curve(y_true, y_score)
+        ax2.plot(fpr, tpr)
+        ax2.plot(fpr, tpr, label="{}-{:.2f}".format(index, auc))
+        ax2.set_title("ROC Curve")
+        ax2.legend()
+
+    plt.show()
+
+
